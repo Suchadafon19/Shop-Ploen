7f58d801d5998c18651fa222196ae4a6349429a4
KanokwanFu <kanokwan.fu@mail.kmutt.ac.th>
2018-11-15 22:36:36 +0700

    create nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,29 @@
 <template>
+  <div id="app">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="product.css">
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Shoppearn</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="product.html">หน้าหลัก</a></li>
+          <li><a href="basket.html">ตะกร้าสินค้า</a></li>
+          <li class="pagenumber"><a class="pagenumber" data-toggle="pagenumber" href="address.html">จัดส่งสินค้า</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> เข้าสู่ระบบ</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <router-view/>
-
+    <!-- <router-view/> -->
+  
 </template>
 
 <style>
