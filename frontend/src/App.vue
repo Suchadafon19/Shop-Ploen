59b32b0bb95dbcddd8fe321270bfa6809b3afa0c
KanokwanFu <kanokwan.fu@mail.kmutt.ac.th>
2018-11-15 22:07:19 +0700

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="product.css">
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Shoppearn</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="product.html">หน้าหลัก</a></li>
+          <li><a href="basket.html">ตะกร้าสินค้า</a></li>
+          <li class="pagenumber"><a class="pagenumber" data-toggle="pagenumber" href="address.html">จัดส่งสินค้า</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> เข้าสู่ระบบ</a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
