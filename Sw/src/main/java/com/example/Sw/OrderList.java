10110f038bd284dc00eac404d927dfd9b08bcf0b
KanokwanFu <kanokwan.fu@mail.kmutt.ac.th>
2018-11-17 14:51:41 +0700

    [CREATE] OrderList + Repository

---
+++ Sw/src/main/java/com/example/Sw/OrderList.java
@@ -0,0 +1,59 @@
+package com.example.Sw;
+
+// import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "OrderList")
+public class OrderList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orderNo")
+    private Long orderNo;
+    @Column(name = "totalPrice")
+    private double totalPrice;
+    // @Column(name = "date")
+    // private Date date;
+    @Column(name = "userNo")
+    private Long userNo;
+
+    public OrderList() {
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    // public Date getDate() {
+    //     return date;
+    // }
+
+    // public void setDate(Date date) {
+    //     this.date = date;
+    // }
+
+    public Long getUserNo() {
+        return userNo;
+    }
+
+    public void setUserNo(Long userNo) {
+        this.userNo = userNo;
+    }
+}
