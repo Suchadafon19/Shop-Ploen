f60453bb5e304822f6b484eae77c64b6f82e2325
l3owNeko <bow.snowich@gmail.com>
2018-11-19 22:01:49 +0700

    [Edit] change @CrossOrigin

--- Sw/src/main/java/com/example/Sw/UserController.java
+++ Sw/src/main/java/com/example/Sw/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*",allowCredentials = "true")
 public class UserController {
     @Autowired
     UserService userService;
