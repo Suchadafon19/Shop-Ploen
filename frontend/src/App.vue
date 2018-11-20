d9a498ad0cbe0cd28b001a303a55cbd1b4091bd4
59130500085 <wipaporn.tec@mail.kmutt.ac.th>
2018-11-20 12:28:33 +0700

    [update]header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@
       <nav class="navbar navbar-inverse">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="/home">Shopploen</a>
+          <h3 style="color:white">Shopploen</h3>
         </div>
         
         <ul class="nav navbar-nav">
